cd93d69d8f206141da0e963683f0f5dfdd82b4ce
unknown <antonkras2012@gmail.com>
2021-05-26 17:18:43 +0400

    media

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,15 @@ document.addEventListener("DOMContentLoaded", function(){
       document.querySelector('#openModal3').style.marginLeft = '0px';
     });
   });
+  document.addEventListener("DOMContentLoaded", function(){
+    var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+    console.log(scrollbar);
+    document.querySelector('[href="#openModal2"]').addEventListener('click',function(){
+      document.body.style.overflow = 'hidden';
+      document.querySelector('#openModal2').style.marginLeft = scrollbar;
+    });
+    document.querySelector('[href="#close2"]').addEventListener('click',function(){
+      document.body.style.overflow = 'visible';
+      document.querySelector('#openModal2').style.marginLeft = '0px';
+    });
+  });
